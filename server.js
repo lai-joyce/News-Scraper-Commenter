f0437e87e7ca89227e6a7ea698ae8dacc842cca7
Joyce Lai <lai.joyce@gmail.com>
2017-09-15 20:29:28 -0700

    fixed PORT for deployment

--- server.js
+++ server.js
@@ -222,7 +222,8 @@ app.delete("/api/comments/:id", function(req, res) {
   });
   });
 
+var PORT = process.env.PORT || 3000;
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+// app.listen(3000, function() {
+//   console.log("App running on port 3000!");
+// });
